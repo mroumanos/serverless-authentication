adbeb858b6046ba1ccc1d09414ae5573ea2d6086
Michael Roumanos <michael.roumanos@thehumangeo.com>
2017-07-28 10:39:07 -0400

    removed dynamodb objects -- not required

--- auth/handler.py
+++ auth/handler.py
@@ -7,41 +7,27 @@ from jwcrypto import jwk, jwt, jws
 
 def auth(event, context):
 
-    bodyTemplate = '{0}: {1} ({2})'
+    bodyTemplate = '[{0}] {1} ({2})'
 
     try:
         # retrieve cookie from requester
-        token = event['headers']['Cookie']
+        token = event['headers']['X-rft-gs-authorization']
 
         # verify that the token
         with open('auth/key.json', 'rb') as file:
             key = jwk.JWK(**json.loads(file.read()))
             verified = jwt.JWT(key=key, jwt=token.split('=')[1])
 
-        # verify session token has been issued to dynamo table (optional)
-        # dynamodb = boto3.resource('dynamodb')
-        # claims = json.loads(verified.claims)
-        # table = dynamodb.Table(os.environ['table'])
-        # item = table.get_item(
-        #     Key={
-        #         'jti': claims['jti']
-        #     }
-        # )
-        # if len(item['Item']) == 1 : # if no entries were found, it returns an empty list
-        #     return { 'statusCode' : 202, 'body' : bodyTemplate.format('Authorized', 'welcome to RFT', str(verified.claims)) }
-        # else:
-        #     raise ClientError
-
     except KeyError as e:
-        return {'statusCode' : 404, 'body' : bodyTemplate.format('Unauthorized', 'no session cookie found', str(e))}
+        raise Exception(bodyTemplate.format(401, 'Unauthorized: no session cookie found', str(e)))
 
     except (jwk.InvalidJWKValue, jwk.InvalidJWKOperation, jwk.InvalidJWKUsage, jws.InvalidJWSSignature, jws.InvalidJWSObject, jws.InvalidJWSOperation) as e:
-        return {'statusCode' : 404, 'body' : bodyTemplate.format('Unauthorized', 'token not valid', str(e))}
+        raise Exception(bodyTemplate.format(401, 'Unauthorized: token not valid', str(e)))
 
     except ClientError as e:
-        return {'statusCode' : 404, 'body' : bodyTemplate.format('Unauthorized', 'table entry not valid', str(e))}
+        raise Exception(bodyTemplate.format(401, 'Unauthorized: table entry not valid', str(e)))
 
     except:
-        return {'statusCode': 404, 'body': bodyTemplate.format('Unauthorized', 'login error', 'unk')}
+        raise Exception(bodyTemplate.format(401, 'Unauthorized: unknown error', 'unknown'))
 
-    return {'statusCode': 202, 'body': bodyTemplate.format('Authorized', 'welcome to RFT', str(verified.claims))}
+    raise Exception(bodyTemplate.format(202, 'Authorized: welcome!', str(token.claims)))
--- login/handler.py
+++ login/handler.py
@@ -48,31 +48,18 @@ def login(event, context):
             token = jwt.JWT(header={"alg": "ES256"}, claims=claims)
             token.make_signed_token(key)
 
-        # dynamodb = boto3.resource('dynamodb')
-        #
-        # table = dynamodb.Table(os.environ['table'])
-        # item = {
-        #     'jti': jti,
-        #     'session': {
-        #         'user': uid,
-        #         'created_at': time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(start)),
-        #         'expires_at': time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(end))
-        #     }
-        # }
-        # table.put_item(Item=item)
-
-        cookie_token = 'rft-gs-authorization={}; domain=rft.geointservices.io; expires={};'.format(token.serialize(),time.strftime('%a, %d %b %y %H:%M:%S',time.gmtime(end)))
+        cookie_token = 'rft_gs_authorization={}; domain=rft.geointservices.io; expires={};'.format(token.serialize(),time.strftime('%a, %d %b %y %H:%M:%S',time.gmtime(end)))
 
     except ldap.LDAPError as e:
-        return { 'location': os.environ['loginurl'], 'body' : bodyTemplate.format('Unauthorized', 'LDAP error', str(e)) }
+        return { 'location': 'https://' + os.environ['loginurl'], 'body' : bodyTemplate.format('Unauthorized', 'LDAP error', str(e)) }
 
     except (jwk.InvalidJWKValue, jwk.InvalidJWKOperation, jwk.InvalidJWKUsage, jws.InvalidJWSSignature, jws.InvalidJWSObject, jws.InvalidJWSOperation) as e:
-        return { 'location': os.environ['loginurl'], 'body': bodyTemplate.format('Unauthorized', 'JWT error', str(e)) }
+        return { 'location': 'https://' + os.environ['loginurl'], 'body': bodyTemplate.format('Unauthorized', 'JWT error', str(e)) }
 
     except ClientError as e:
-        return {'location': os.environ['loginurl'], 'body' : bodyTemplate.format('Unauthorized', 'Dynamo error', str(e)) }
+        return {'location': 'https://' + os.environ['loginurl'], 'body' : bodyTemplate.format('Unauthorized', 'Dynamo error', str(e)) }
 
     except:
-        return { 'location': os.environ['loginurl'], 'body': 'Unknown error' }
+        return { 'location': 'https://' + os.environ['loginurl'], 'body': 'Unknown error' }
 
-    return {'location': 'https://home.rft.geointservices.io', 'cookie': cookie_token }
+    return { 'location': 'https://home.rft.geointservices.io', 'cookie': cookie_token }
