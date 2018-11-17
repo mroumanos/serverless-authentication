ec33414dd7b65afa98015f49ef26fc8badd0bde5
Michael Roumanos <michael.roumanos@thehumangeo.com>
2018-11-16 21:23:15 -0500

    update gitignore

--- authEval/handler.py
+++ authEval/handler.py
@@ -24,17 +24,14 @@ def auth(event, context):
     except (jwk.InvalidJWKValue, jwk.InvalidJWKOperation, jwk.InvalidJWKUsage, jws.InvalidJWSSignature, jws.InvalidJWSObject, jws.InvalidJWSOperation) as e:
         raise Exception(bodyTemplate.format(401, 'Unauthorized: token not valid', str(e)))
 
-    except:
-        raise Exception(bodyTemplate.format(401, 'Unauthorized: unknown error', 'token=' + str(token) + ' event=' + str(event)))
+    except Exception as e:
+        raise Exception(bodyTemplate.format(401, 'Unauthorized: unknown error #1', 'token={}, event={}, exception={}'.format(str(token), str(event), str(e))))
 
     try:
         s3 = boto3.resource('s3')
         obj = s3.Object(os.environ['policyBucket'], '{}/{}'.format(os.environ['policyPath'],os.environ['policyFile']))
         policy = json.loads(obj.get()['Body'].read())
-        print policy
         username = json.loads(verified.claims)['prn']
-        print username
-        print(policy[username])
         profile = policy[username] # will throw error if not there
 
     except ClientError as e:
@@ -43,7 +40,7 @@ def auth(event, context):
     except KeyError as e:
         raise Exception(bodyTemplate.format(401, 'Unauthorized: username not whitelisted ({})'.format(username), str(e)))
 
-    except:
-        raise Exception(bodyTemplate.format(401, 'Unauthorized: unknown error', 'token=' + str(token) + ' event=' + str(event)))
+    except Exception as e:
+        raise Exception(bodyTemplate.format(401, 'Unauthorized: unknown error #2', 'token={}, event={}, exception={}'.format(str(token),str(event),str(e))))
 
     raise Exception(bodyTemplate.format(202, 'Authorized: welcome!', 'profile={} token={}'.format(profile,verified.claims)))
